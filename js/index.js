c56d0e386d0103be297bdd283f729e2db8509c46
pfnas <103249634+pfnas@users.noreply.github.com>
2023-11-08 09:55:56 +0100

    correction couleur logo

---
+++ js/index.js
@@ -0,0 +1,8 @@
+const clickCamion = document.querySelector(".loader");
+console.log(clickCamion)
+
+clickCamion.addEventListener("click", eventAdd)
+
+function eventAdd(){
+    alert("voici mon camion")
+}
